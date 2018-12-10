869f66ba5f5d05c4ac68082d324e7f5cc5af139b
LeMinhDuong <leminhduongvn@gmail.com>
2018-12-10 02:58:49 +0000

    commit

--- src/server/app.js
+++ src/server/app.js
@@ -14,6 +14,27 @@ var con = mysql.createConnection({
   database: "web" // use the specified database
 });
 
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(con);                                               
+  connection.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                                                            
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                  
+    }
+  });
+}
+handleDisconnect();
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
